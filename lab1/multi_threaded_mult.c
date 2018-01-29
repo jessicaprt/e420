10432eaea163c23e9c41857d9b83e6454ab6dace
George Antonious <george@antonious.ca>
2018-01-29 11:52:48 -0700

    Scafold the main file

---
+++ lab1/multi_threaded_mult.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lab1_IO.h"
+#include "timer.h"
+
+int main (int argc, char* argv[])
+{
+    if (argc < 2) {
+        printf("Usage %s TOTAL_CORES", argv[0]);
+        return -1;
+    }
+
+    int n; // matrix size
+    int total_cores = atoi(argv[1]);
+    int start_time, end_time; // time measurement
+    int** A;
+    int** B;
+    int** C; // loaded matricies
+
+    // load input
+    Lab1_loadinput(&A, &B, &n);
+
+    GET_TIME(start_time);
+    // do the maths
+    GET_TIME(end_time);
+
+    // save output
+    Lab1_saveoutput(C, &n, end_time - start_time);
+}
