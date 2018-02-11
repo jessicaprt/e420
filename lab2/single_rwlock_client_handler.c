526ad2c989a26d6cf8ffa7d87cd6849b75db9cca
George Antonious <george@antonious.ca>
2018-02-10 17:56:07 -0700

    Add client handlers for the rw lock

---
+++ lab2/single_rwlock_client_handler.c
@@ -0,0 +1,45 @@
+#include <pthread.h>
+#include <string.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include "client_handler.h"
+#include "common.h"
+#include "read_write_lock.h"
+
+rwlock_t rw_lock;
+int LENGTH_OF_MODIFIED_LINE_WITHOUT_INDEX = 45;
+
+void set_up_client_handler(int array_length) {
+    rwlock_init(&rw_lock);
+}
+
+void* handle_client(void* handle_client_params) {
+    handle_client_params_t* params = (handle_client_params_t*) handle_client_params;
+
+    int request_type = receive_int(params->socket);
+    int request_index = receive_int(params->socket);
+
+    if (request_type == READ_REQUEST_TYPE) {
+        rwlock_rlock(&rw_lock);
+        char * arrayLine = params->theArray[request_index];
+        rwlock_unlock(&rw_lock);
+
+        int arrayLength = strlen(arrayLine);
+        send_chars(params->socket, arrayLine, arrayLength);
+    } else if (request_type == WRITE_REQUEST_TYPE) {
+        rwlock_wlock(&rw_lock);
+        int newLength = LENGTH_OF_MODIFIED_LINE_WITHOUT_INDEX + get_total_digits_of(request_index);
+        params->theArray[request_index] = realloc(params->theArray[request_index], newLength * sizeof(char));
+        snprintf(params->theArray[request_index], newLength, "String %d has been modified by a write request", request_index);
+        rwlock_unlock(&rw_lock);
+
+        rwlock_rlock(&rw_lock);
+        char * arrayLine = params->theArray[request_index];
+        rwlock_unlock(&rw_lock);
+
+        int arrayLength = strlen(arrayLine);
+        send_chars(params->socket, arrayLine, arrayLength);
+    }
+
+    return NULL;
+}
