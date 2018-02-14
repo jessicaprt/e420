6c237100964f894fbaf4da24ce07e913d5c444ec
George Antonious <george@antonious.ca>
2018-02-14 09:23:20 -0700

    Get graphs generating

--- lab2/stats_gen.py
+++ lab2/stats_gen.py
@@ -1,3 +1,8 @@
+import pandas
+from  matplotlib import pyplot
+import matplotlib.pyplot as plt
+import seaborn
+
 SERVER_EXECUTABLES = [
     "server_single_mutex",
     "server_multiple_mutex",
@@ -11,14 +16,30 @@ TEST_RESULTS_DIR = "test-results"
 OUT_DIR = "stats"
 output = []
 
+def gen_client_graph(server_executable, array_size, data):
+    seaborn.set(style='ticks')
+
+    plot = seaborn.distplot(data, hist_kws=dict(cumulative=True), kde_kws=dict(cumulative=True))
+    figure = plot.get_figure()
+
+    figure.suptitle("{} with an array size of {}".format(server_executable, array_size), fontsize = 16)
+    plt.xlabel('Client Latency (ms)', fontsize=12)
+    plt.ylabel('Cumulative Probability', fontsize=12)
+
+    figure.savefig("./{}/{}-{}.png".format(OUT_DIR, server_executable, array_size), dpi=400)
+    figure.clf()
+
 for server_executable in SERVER_EXECUTABLES:
     for array_size in ARRAY_SIZES:
         client_latency_file = "./{}/{}-{}.txt".format(TEST_RESULTS_DIR, server_executable, array_size)
-        sum = 0
+        mean = 0
+
         with open(client_latency_file) as client_file:
-            for line in client_file:
-                sum += float(line.strip())
-        output.append("{}-{} mean = {} ms".format(server_executable, array_size, sum / float(TOTAL_RUNS)))
+            client_data = [float(line.strip()) for line in client_file.readlines()]
+            mean = sum(client_data) / float(len(client_data))
+            gen_client_graph(server_executable, array_size, client_data)
+
+        output.append("{}-{} mean = {} ms".format(server_executable, array_size, mean))
 
 with open("./{}/summary.txt".format(OUT_DIR), 'w') as summary_file:
     for line in output:
