907b408250496609650991beabfe349e3d883cc4
George Antonious <george@antonious.ca>
2018-02-13 20:25:13 -0700

    Update client

--- lab2/client.c
+++ lab2/client.c
@@ -7,8 +7,10 @@
 #include<unistd.h>
 #include<pthread.h>
 #include "common.h"
+#include "timer.h"
 
 int array_size;
+const int NUMBER_REQUESTS = 1000;
 
 int get_random_int(int max) {
 	return rand() % (max + 1);
@@ -20,7 +22,7 @@ void* request_handler(void* socket_param) {
 	int request_type;
 	int request_chance = get_random_int(100);
 
-	if (request_chance < 15) {
+	if (request_chance < 5) {
 		request_type = WRITE_REQUEST_TYPE;
 	} else {
 		request_type = READ_REQUEST_TYPE;
@@ -54,22 +56,37 @@ int main(int argc, char** argv) {
 	srand((int) &seed);
 
 	struct sockaddr_in sock_var;
-	int clientFileDescriptor=socket(AF_INET, SOCK_STREAM, 0);
 
 	sock_var.sin_addr.s_addr=inet_addr("127.0.0.1");
 	sock_var.sin_port= htons(port_number);
 	sock_var.sin_family=AF_INET;
 
-	int connectionResult = connect(clientFileDescriptor, (struct sockaddr*) &sock_var, sizeof(sock_var));
-	if (connectionResult < 0) {
-		perror("socket creation failed");
-		return -1;
+	pthread_t* thread_handles = malloc(NUMBER_REQUESTS * sizeof(pthread_t));
+
+	double start_time, end_time;
+
+	GET_TIME(start_time);
+
+	int i = 0;
+	for (i = 0; i < NUMBER_REQUESTS; i++) {
+		int clientFileDescriptor=socket(AF_INET, SOCK_STREAM, 0);
+		int connectionResult = connect(clientFileDescriptor, (struct sockaddr*) &sock_var, sizeof(sock_var));
+
+		if (connectionResult < 0) {
+			perror("socket creation failed");
+			return -1;
+		}
+
+		pthread_create(&thread_handles[i], NULL, request_handler, (void*) clientFileDescriptor);
 	}
 
-	pthread_t* client_thread = malloc(sizeof(pthread_t));
+	for (i = 0; i < NUMBER_REQUESTS; i++) {
+		pthread_join(thread_handles[i], NULL);
+	}
 
-	pthread_create(client_thread, NULL, request_handler, (void*) clientFileDescriptor);
-	pthread_join(*client_thread, NULL);
+	GET_TIME(end_time);
+
+	printf("Elapsed time: %f\n", end_time - start_time);
 
 	return 0;
 }
