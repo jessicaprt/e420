6bbcc30e47746735f6336a1362fb7a80bc307f3e
George Antonious <george@antonious.ca>
2018-02-14 11:39:31 -0700

    Add test results

--- lab2/stats_gen.py
+++ lab2/stats_gen.py
@@ -19,10 +19,15 @@ output = []
 def gen_client_graph(server_executable, array_size, data):
     seaborn.set(style='ticks')
 
-    plot = seaborn.distplot(data, hist_kws=dict(cumulative=True), kde_kws=dict(cumulative=True))
+    plot = seaborn.distplot(data, color='r', hist_kws=dict(cumulative=True), kde_kws=dict(cumulative=True))
     figure = plot.get_figure()
 
     figure.suptitle("{} with an array size of {}".format(server_executable, array_size), fontsize = 16)
+
+    axis = figure.gca()
+    axis.set_autoscale_on(False)
+    axis.axis([0, 0.4, 0, 1.1])
+
     plt.xlabel('Client Latency (ms)', fontsize=12)
     plt.ylabel('Cumulative Probability', fontsize=12)
 
@@ -32,10 +37,15 @@ def gen_client_graph(server_executable, array_size, data):
 def gen_server_graph(server_executable, array_size, data):
     seaborn.set(style='ticks')
 
-    plot = seaborn.distplot(data, hist_kws=dict(cumulative=True), kde_kws=dict(cumulative=True))
+    plot = seaborn.distplot(data, color='g' ,hist_kws=dict(cumulative=True), kde_kws=dict(cumulative=True))
     figure = plot.get_figure()
 
     figure.suptitle("{} with an array size of {}".format(server_executable, array_size), fontsize = 16)
+
+    axis = figure.gca()
+    axis.set_autoscale_on(False)
+    axis.axis([0, 0.035, 0, 1.1])
+
     plt.xlabel('Server Memory Access Latency (ms)', fontsize=12)
     plt.ylabel('Cumulative Probability', fontsize=12)
 
@@ -47,7 +57,7 @@ for server_executable in SERVER_EXECUTABLES:
         client_latency_file = "./{}/{}-client-{}.txt".format(TEST_RESULTS_DIR, server_executable, array_size)
         server_latency_file = "./{}/{}-server-{}.txt".format(TEST_RESULTS_DIR, server_executable, array_size)
         client_mean = 0
-        server_mean
+        server_mean = 0
 
         with open(client_latency_file) as client_file:
             client_data = [float(line.strip()) for line in client_file.readlines() if float(line.strip()) < 1]
@@ -55,12 +65,12 @@ for server_executable in SERVER_EXECUTABLES:
             gen_client_graph(server_executable, array_size, client_data)
 
         with open(server_latency_file) as server_file:
-            server_data = [float(line.strip()) for line in client_file.readlines() if float(line.strip()) < 1]
+            server_data = [float(line.strip()) for line in server_file.readlines() if float(line.strip()) < 1]
             server_mean = sum(server_data) / float(len(server_data))
             gen_server_graph(server_executable, array_size, server_data)
 
-        output.append("{}-{} client mean = {} ms".format(server_executable, array_size, client_mean))
-        output.append("{}-{} server mean = {} ms".format(server_executable, array_size, server_mean))
+        output.append("{}-{} client mean = {:.3f} ms".format(server_executable, array_size, client_mean))
+        output.append("{}-{} server mean = {:.3f} ms".format(server_executable, array_size, server_mean))
 
 with open("./{}/summary.txt".format(OUT_DIR), 'w') as summary_file:
     for line in output:
