c3937179492a05e76b1bcb2f550a4c90d326c822
George Antonious <george@antonious.ca>
2018-03-08 10:15:01 -0700

    add fig

--- lab3/stats_gen.py
+++ lab3/stats_gen.py
@@ -16,62 +16,28 @@ TEST_RESULTS_DIR = "test-results"
 OUT_DIR = "stats"
 output = []
 
-def gen_client_graph(server_executable, array_size, data):
-    seaborn.set(style='ticks')
-
-    plot = seaborn.distplot(data, color='r', hist_kws=dict(cumulative=True), kde_kws=dict(cumulative=True))
-    figure = plot.get_figure()
-
-    figure.suptitle("{} with an array size of {}".format(server_executable, array_size), fontsize = 16)
-
-    axis = figure.gca()
-    axis.set_autoscale_on(False)
-    axis.axis([0, 0.4, 0, 1.1])
-
-    plt.xlabel('Client Latency (ms)', fontsize=12)
-    plt.ylabel('Cumulative Probability', fontsize=12)
-
-    figure.savefig("./{}/{}-client-{}.png".format(OUT_DIR, server_executable, array_size), dpi=400)
-    figure.clf()
-
-def gen_server_graph(server_executable, array_size, data):
-    seaborn.set(style='ticks')
-
-    plot = seaborn.distplot(data, color='g' ,hist_kws=dict(cumulative=True), kde_kws=dict(cumulative=True))
-    figure = plot.get_figure()
-
-    figure.suptitle("{} with an array size of {}".format(server_executable, array_size), fontsize = 16)
-
-    axis = figure.gca()
-    axis.set_autoscale_on(False)
-    axis.axis([0, 0.035, 0, 1.1])
-
-    plt.xlabel('Server Memory Access Latency (ms)', fontsize=12)
-    plt.ylabel('Cumulative Probability', fontsize=12)
-
-    figure.savefig("./{}/{}-server-{}.png".format(OUT_DIR, server_executable, array_size), dpi=400)
-    figure.clf()
-
 def gen_graph(data):
-    df2 = pd.DataFrame(np.random.rand(10, 4), columns=['a', 'b', 'c', 'd'])
-    df2.plot.bar()
+    plt.figure()
+    df2 = pandas.DataFrame(data, columns=EXECUTABLES)
+    ax = df2.plot.bar(xticks = THREAD_SIZES)
+
+    plt.title('Performance of Different Parallelization Strategies', fontsize=12)
+    plt.xlabel('Number of Threads', fontsize=12)
+    plt.ylabel('Execution Time', fontsize=12)
+
+    plt.savefig('./fig.png')
 
 
 means = []
-for thread_size in enumerate(THREAD_SIZES):
+for thread_size in THREAD_SIZES:
     means.append([])
     for executable in EXECUTABLES:
         latency_file = "./{}/{}-{}.txt".format(TEST_RESULTS_DIR, executable, thread_size)
         mean_latency = 0
 
         with open(latency_file) as file:
-            data = [float(line.strip()) for line in file.readlines() if float(line.strip()) < 1]
-            mean_latency = sum(client_data) / float(len(client_data))
+            data = [float(line.strip()) for line in file.readlines()]
+            mean_latency = sum(data) / float(len(data))
             means[-1].append(mean_latency)
 
-gen_graph(data)
-
-
-with open("./{}/summary.txt".format(OUT_DIR), 'w') as summary_file:
-    for line in output:
-        summary_file.write(line + "\n")
+gen_graph(means)
