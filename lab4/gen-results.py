e5082a6eddc710fcea1397ff4f0761605e411ba6
George Antonious <george@antonious.ca>
2018-04-04 23:38:58 -0600

    Get test results

--- lab4/gen-results.py
+++ lab4/gen-results.py
@@ -19,11 +19,11 @@ exec_command("rm {0}/*".format(OUT_DIR))
 exec_command("mkdir {0}".format(OUT_DIR))
 
 for data_size in DATA_SIZES:
-    print("Testing {} with a thread size of {}.".format(EXECUTABLE, data_size))
-    print("Generating data with size of {}".format(DATA_SIZES))
+    print("Generating data with size of {}".format(data_size ))
     exec_command("./{} -b {}".format(DATA_GEN, data_size))
     for proc_size in PROC_SIZES:
+        print("Testing {} with a data size of {} and process count of {}.".format(EXECUTABLE, data_size, proc_size))
         for i in range(TOTAL_RUNS):
             results_file = "./{0}/{1}-{2}-{3}.txt".format(OUT_DIR, EXECUTABLE, data_size, proc_size)
-            launch_command = "mpirun -np {} ./{} | tail -n 1 >> {}".format(proc_size, EXECUTABLE, results_file)
+            launch_command = "mpirun -np {} -f hosts ./{} | tail -n 1 >> {}".format(proc_size, EXECUTABLE, results_file)
             process = exec_command(launch_command)
