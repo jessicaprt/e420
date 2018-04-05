14499c09f65ee22da80847538b8bc0757f40f5e9
George Antonious <george@antonious.ca>
2018-04-04 23:23:42 -0600

    Update results generator

--- lab4/gen-results.py
+++ lab4/gen-results.py
@@ -6,7 +6,7 @@ EXECUTABLE = "main"
 DATA_GEN = "datatrim"
 
 TOTAL_RUNS = 10
-THREAD_SIZE = [1, 4, 8, 16]
+PROC_SIZES = [1, 4, 8, 16]
 DATA_SIZES = [4000, 8000, 16000]
 
 def start_command(command):
@@ -18,14 +18,12 @@ def exec_command(command):
 exec_command("rm {0}/*".format(OUT_DIR))
 exec_command("mkdir {0}".format(OUT_DIR))
 
-if (not os.path.exists("data_input")):
-    exec_command("./{} -s {}".format(DATA_GEN, DATA_SIZES))
-
 for data_size in DATA_SIZES:
     print("Testing {} with a thread size of {}.".format(EXECUTABLE, data_size))
     print("Generating data with size of {}".format(DATA_SIZES))
-    exec_command("./{} -s {}".format(DATA_GEN, DATA_SIZES))
-    for i in range(TOTAL_RUNS):
-        results_file = "./{0}/{1}-{2}.txt".format(OUT_DIR, EXECUTABLE, data_size)
-        launch_command = "mpirun -np {} ./{} | tail -n 1 >> {}".format(data_size, EXECUTABLE, results_file)
-        process = exec_command(launch_command)
+    exec_command("./{} -b {}".format(DATA_GEN, data_size))
+    for proc_size in PROC_SIZES:
+        for i in range(TOTAL_RUNS):
+            results_file = "./{0}/{1}-{2}-{3}.txt".format(OUT_DIR, EXECUTABLE, data_size, proc_size)
+            launch_command = "mpirun -np {} ./{} | tail -n 1 >> {}".format(proc_size, EXECUTABLE, results_file)
+            process = exec_command(launch_command)
