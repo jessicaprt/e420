fb4494f30c3a5a8260761ae79ca869d94ec98127
George Antonious <george@antonious.ca>
2018-04-04 22:33:52 -0600

    Make push is the best

--- lab4/gen-results.py
+++ lab4/gen-results.py
@@ -1,7 +1,6 @@
 import subprocess, time
 import os.path
 
-
 OUT_DIR = "test-results"
 EXECUTABLE = "main"
 DATA_GEN = "datatrim"
@@ -20,13 +19,13 @@ exec_command("rm {0}/*".format(OUT_DIR))
 exec_command("mkdir {0}".format(OUT_DIR))
 
 if (not os.path.exists("data_input")):
-    exec_command("./datagen -s 2048")
-
+    exec_command("./{} -s {}".format(DATA_GEN, DATA_SIZES))
 
 for data_size in DATA_SIZES:
     print("Testing {} with a thread size of {}.".format(EXECUTABLE, data_size))
-
+    print("Generating data with size of {}".format(DATA_SIZES))
+    exec_command("./{} -s {}".format(DATA_GEN, DATA_SIZES))
     for i in range(TOTAL_RUNS):
-        results_file = "./{0}/{1}-{2}.txt".format(OUT_DIR, executable, thread_size)
-        launch_command = "./{}.out {} | tail -n 1 >> {}".format(executable, thread_size, results_file)
+        results_file = "./{0}/{1}-{2}.txt".format(OUT_DIR, EXECUTABLE, data_size)
+        launch_command = "mpirun -np {} ./{} | tail -n 1 >> {}".format(data_size, EXECUTABLE, results_file)
         process = exec_command(launch_command)
